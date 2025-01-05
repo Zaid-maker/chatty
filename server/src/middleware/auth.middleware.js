02cc1ac64da622903c81811b30c738831f8aa9a9
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-01-05 22:00:32 +0500

    refactor auth middleware error messages for consistency and clarity

--- server/src/middleware/auth.middleware.js
+++ server/src/middleware/auth.middleware.js
@@ -13,13 +13,13 @@ export const protectRoute = async (req, res, next) => {
     const token = req.cookies.jwt;
 
     if (!token) {
-      return res.status(401).json({ message: "Unauthorized - no token found" });
+      return res.status(401).json({ message: "Unauthorized - No Token Provided" });
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     if (!decoded) {
-      return res.status(401).json({ message: "Unauthorized - invalid token" });
+      return res.status(401).json({ message: "Unauthorized - Invalid Token" });
     }
 
     const user = await User.findById(decoded.userId).select("-password");
@@ -28,7 +28,7 @@ export const protectRoute = async (req, res, next) => {
       return res.status(404).json({ message: "User not found" });
     }
 
-    req.user= user;
+    req.user = user;
 
     next();
   } catch (error) {
