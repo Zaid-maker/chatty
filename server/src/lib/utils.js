1228eab1fc7f15249adc8f0e6cdc4f11ae39d01b
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-01-05 22:03:23 +0500

    fix: correct secure cookie setting for production environment

--- server/src/lib/utils.js
+++ server/src/lib/utils.js
@@ -18,7 +18,7 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS in a week (7 days)
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
     sameSite: "strict", // CSRF attacks
-    secure: process.env.NODE_ENV === "development", // cookie only works in https
+    secure: process.env.NODE_ENV !== "development", // cookie only works in https
   });
 
   return token;
