5bffb897fcf1aa357e7ac5458186c7e981b6f650
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-01-06 18:38:40 +0500

    feat: add authentication store with Zustand and integrate Axios for API calls; update Navbar for user logout functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { LogOut, MessageSquare, Settings, User } from "lucide-react";
+import { useAuthStore } from "../store/useAuthStore";
 
 const Navbar = () => {
+  const { logout, authUser } = useAuthStore();
+
   return (
     <header className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg bg-base-100/80">
       <div className="container mx-auto px-4 h-16">
@@ -31,15 +34,19 @@ const Navbar = () => {
               <span className="hidden sm:inline">Settings</span>
             </Link>
 
-            <Link to={"/profile"} className={`btn btn-sm gap-2`}>
-              <User className="size-5" />
-              <span className="hidden sm:inline">Profile</span>
-            </Link>
+            {authUser && (
+              <>
+                <Link to={"/profile"} className={`btn btn-sm gap-2`}>
+                  <User className="size-5" />
+                  <span className="hidden sm:inline">Profile</span>
+                </Link>
 
-            <button className="flex gap-2 items-center">
-              <LogOut className="size-5" />
-              <span className="hidden sm:inline">Logout</span>
-            </button>
+                <button className="flex gap-2 items-center" onClick={logout}>
+                  <LogOut className="size-5" />
+                  <span className="hidden sm:inline">Logout</span>
+                </button>
+              </>
+            )}
           </div>
         </div>
       </div>
---
+++ client/src/store/useAuthStore.js
@@ -0,0 +1,36 @@
+import { create } from 'zustand';
+import { axiosInstance } from '../lib/axios.js';
+
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+
+export const useAuthStore = create((set, get) => ({
+    authUser: null,
+    isSigningUp: false,
+    isLoggingIn: false,
+    isUpdatingProfile: false,
+    isCheckingAuth: true,
+    onlineUsers: [],
+    socket: null,
+
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get("/auth/check");
+
+            set({ authUser: res.data });
+        } catch (error) {
+            console.log("Error in checkAuth", error);
+            set({ authUser: null });
+        } finally {
+            set({ isCheckingAuth: false });
+        }
+    },
+
+    logOut: async () => {
+        try {
+            await axiosInstance.post("/auth/logout");
+            set({ authUser: null });
+        } catch (error) {
+            
+        }
+    }
+}));
