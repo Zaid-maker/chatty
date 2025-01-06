1a568bb17646dab7686f2ccb920654cb57285a8b
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-01-06 21:11:00 +0500

    feat: implement routing for user authentication and settings; add HomePage, SignUpPage, and LoginPage components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,51 @@
 import Navbar from "./components/Navbar";
 
+// import HomePage from "./pages/HomePage";
+// import SignUpPage from "./pages/SignUpPage";
+// import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+// import ProfilePage from "./pages/ProfilePage";
+
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useAuthStore } from "./store/useAuthStore";
+// import { useThemeStore } from "./store/useThemeStore";
+import { useEffect } from "react";
+
+import { Loader } from "lucide-react";
+// import { Toaster } from "react-hot-toast";
+
 function App() {
+  const { authUser, checkAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+
   return (
     <div className="p-4">
       <Navbar />
+
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+      </Routes>
     </div>
   );
 }
