7629b805992ea133f4bc97a04eb5e2267e2815e5
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-01-09 19:24:55 +0500

    feat: enhance authentication flow with success and error notifications

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -15,12 +15,10 @@ export const useAuthStore = create((set, get) => ({
     socket: null,
 
     /**
-     * Checks if a user is authenticated by sending a GET request to the /auth/check route.
-     * If the user is authenticated, sets the authUser state to the user object from the response
-     * and calls the connectSocket function to connect to the socket.io server.
-     * If the user is not authenticated, sets the authUser state to null.
-     * Finally, sets the isCheckingAuth state to false.
-     * @function
+     * Checks if the user is authenticated by making a GET request to /auth/check.
+     * If the user is authenticated, stores the user data in state and connects the socket.
+     * If the user is not authenticated, sets the user data to null and disconnects the socket.
+     * Sets isCheckingAuth to false when the operation is complete.
      */
     checkAuth: async () => {
         try {
@@ -37,99 +35,97 @@ export const useAuthStore = create((set, get) => ({
     },
 
     /**
-     * Signs up a user by sending a POST request to the /auth/signup route with the
-     * provided data. If the data is valid, sets the authUser state to the user object
-     * from the response and calls the connectSocket function to connect to the
-     * socket.io server. If the data is invalid, sets the authUser state to null.
-     * @function signUp
-     * @memberof useAuthStore
-     * @instance
-     * @param {Object} data - The user's data, containing a fullName, email, and password.
-     * @returns {Promise<void>} A promise that resolves when the user has been signed up.
+     * Signs up a user and logs them in. If the operation is successful, sets the user data in state and connects the socket.
+     * If the operation fails, displays an error message using toast.
+     * Sets isSigningUp to true until the operation is complete.
+     * @param {Object} data - The data to send in the request body.
+     * @param {string} data.fullName - The user's full name.
+     * @param {string} data.email - The user's email address.
+     * @param {string} data.password - The user's password.
      */
     signup: async (data) => {
-        set({ isSigningUp: true }); 
+        set({ isSigningUp: true });
         try {
             const res = await axiosInstance.post("/auth/signup", data);
             set({ authUser: res.data });
+            toast.success("Account created successfully");
             get().connectSocket();
         } catch (error) {
-            
+            toast.error(error.response.data.message);
         } finally {
             set({ isSigningUp: false });
         }
     },
 
     /**
-     * Logs the user in by sending a POST request to the /auth/login route with the
-     * provided credentials. If the credentials are valid, sets the authUser state to the
-     * user object from the response and calls the connectSocket function to connect
-     * to the socket.io server. If the credentials are invalid, sets the authUser state
-     * to null.
-     * @function logIn
-     * @memberof useAuthStore
-     * @instance
-     * @param {Object} data - The user's credentials, containing a username and password.
-     * @returns {Promise<void>} A promise that resolves when the user has been logged in.
+     * Logs a user in. If the operation is successful, sets the user data in state and connects the socket.
+     * If the operation fails, displays an error message using toast.
+     * Sets isLoggingIn to true until the operation is complete.
+     * @param {Object} data - The data to send in the request body.
+     * @param {string} data.email - The user's email address.
+     * @param {string} data.password - The user's password.
      */
     login: async (data) => {
         set({ isLoggingIn: true });
         try {
             const res = await axiosInstance.post("/auth/login", data);
             set({ authUser: res.data });
+            toast.success("Logged in successfully");
+
             get().connectSocket();
         } catch (error) {
-            
+            toast.error(error.response.data.message);
         } finally {
             set({ isLoggingIn: false });
         }
     },
 
     /**
-     * Logs the user out of the application by sending a request to the
-     * server to destroy the user's session.
-     *
-     * @function logOut
-     * @memberof useAuthStore
-     * @instance
-     * @returns {Promise<void>} A promise that resolves when the user has been logged out.
+     * Logs a user out by making a POST request to /auth/logout.
+     * If the operation is successful, clears the user data from state and disconnects the socket.
+     * If the operation fails, displays an error message using toast.
      */
+
     logout: async () => {
         try {
             await axiosInstance.post("/auth/logout");
             set({ authUser: null });
+            toast.success("Logged out successfully");
+            get().disconnectSocket();
         } catch (error) {
-
+            toast.error(error.response.data.message);
         }
     },
 
     /**
-     * Updates the user's profile by sending a request to the server to
-     * update the user's profile.
-     *
-     * @function updateProfile
-     * @memberof useAuthStore
-     * @instance
-     * @param {Object} data The data to update the user's profile with.
-     * @returns {Promise<void>} A promise that resolves when the user's profile has been updated.
+     * Updates the user's profile with the provided data by making a PUT request to /auth/update-profile.
+     * If the operation is successful, updates the user data in state and displays a success message.
+     * If the operation fails, logs the error and displays an error message using toast.
+     * Sets isUpdatingProfile to true until the operation is complete.
+     * @param {Object} data - The data to send in the request body.
      */
+
     updateProfile: async (data) => {
         set({ isUpdatingProfile: true });
         try {
             const res = await axiosInstance.put("/auth/update-profile", data);
             set({ authUser: res.data });
+            toast.success("Profile updated successfully");
         } catch (error) {
-            
+            console.log("error in update profile:", error);
+            toast.error(error.response.data.message);
         } finally {
             set({ isUpdatingProfile: false });
         }
     },
 
     /**
-     * Connects to the socket.io server and sets up the socket event listeners.
-     * Listens for the "getOnlineUsers" event and updates the onlineUsers state
-     * when a new list of online user IDs is received.
-     * @function
+     * Connects the socket to the server by making a connection to the WebSocket endpoint
+     * at /socket.io. If the user is not authenticated or the socket is already connected,
+     * does nothing.
+     * Sets the socket instance in state and listens for the "getOnlineUsers" event.
+     * When the event is received, updates the onlineUsers state with the received array
+     * of user IDs.
      */
     connectSocket: () => {
         const { authUser } = get();
@@ -148,12 +144,12 @@ export const useAuthStore = create((set, get) => ({
             set({ onlineUsers: userIds });
         });
     },
-
     /**
-     * Disconnects from the socket.io server.
-     * @function
+     * Disconnects the socket connection if it is currently connected.
+     * This method checks the current socket state and performs the disconnect operation.
      */
+
     disconnectSocket: () => {
         if (get().socket?.connected) get().socket.disconnect();
-    }
+    },
 }));
