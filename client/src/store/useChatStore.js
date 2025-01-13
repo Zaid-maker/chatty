5de6d5d23db1b71d1bf73f3b606bcf084717d679
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-01-13 23:54:13 +0500

    fix: streamline message sending logic and correct unsubscribe method name in useChatStore

--- client/src/store/useChatStore.js
+++ client/src/store/useChatStore.js
@@ -37,10 +37,7 @@ export const useChatStore = create((set, get) => ({
   sendMessage: async (messageData) => {
     const { selectedUser, messages } = get();
     try {
-      const res = await axiosInstance.post(
-        `/messages/send/${selectedUser._id}`,
-        messageData,
-      );
+      const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
       set({ messages: [...messages, res.data] });
     } catch (error) {
       toast.error(error.response.data.message);
@@ -54,8 +51,7 @@ export const useChatStore = create((set, get) => ({
     const socket = useAuthStore.getState().socket;
 
     socket.on("newMessage", (newMessage) => {
-      const isMessageSentFromSelectedUser =
-        newMessage.senderId === selectedUser._id;
+      const isMessageSentFromSelectedUser = newMessage.senderId === selectedUser._id;
       if (!isMessageSentFromSelectedUser) return;
 
       set({
@@ -64,7 +60,7 @@ export const useChatStore = create((set, get) => ({
     });
   },
 
-  ubsubscribeToMessages: () => {
+  unsubscribeFromMessages: () => {
     const socket = useAuthStore.getState().socket;
     socket.off("newMessage");
   },
