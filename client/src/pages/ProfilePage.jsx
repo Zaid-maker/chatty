214965568e5a64d23673ee4e04a9d6b66fc6f8de
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-01-14 17:49:55 +0500

    feat: display user's full name and email address on profile page

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
+import { Mail, User } from "lucide-react";
 
 const ProfilePage = () => {
   const { authUser, isUpdatingProfile, updateProfile } = useAuthStore();
@@ -45,6 +46,28 @@ const ProfilePage = () => {
                 ? "Uploading..."
                 : "Click the camera icon to update your photo"}
             </p>
+          </div>
+
+          <div className="space-y-6">
+            <div className="space-y-1.5">
+              <div className="text-sm text-zinc-400 flex items-center gap-2">
+                <User className="w-4 h-4" />
+                Full Name
+              </div>
+              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">
+                {authUser?.fullName}
+              </p>
+            </div>
+
+            <div className="space-y-1.5">
+              <div className="text-sm text-zinc-400 flex items-center gap-2">
+                <Mail className="w-4 h-4" />
+                Email Address
+              </div>
+              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">
+                {authUser?.email}
+              </p>
+            </div>
           </div>
         </div>
       </div>
