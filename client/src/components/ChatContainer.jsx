5b7d4c41e1d71e906c12c6d65bc480c1ae503689
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-01-17 21:50:34 +0500

    feat: enhance ChatContainer with message fetching and subscription logic

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+import { useChatStore } from "../store/useChatStore";
+import { useAuthStore } from "../store/useAuthStore";
 
 const ChatContainer = () => {
-  return (
-    <div>ChatContainer</div>
-  )
-}
+  const {
+    messages,
+    getMessages,
+    isMessagesLoading,
+    selectedUser,
+    subscribeToMessages,
+    unsubscribeFromMessages,
+  } = useChatStore();
+  const { authUser } = useAuthStore();
+  const messageEndRef = useRef(null);
 
-export default ChatContainer
+  useEffect(() => {
+    getMessages(selectedUser._id);
+
+    subscribeToMessages();
+  }, [
+    selectedUser._id,
+    getMessages,
+    subscribeToMessages,
+    unsubscribeFromMessages,
+  ]);
+
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <div className="flex-1 overflow-y-auto p-4 space-y-4">
+        {messages.map((message) => (
+          <div
+            key={message._id}
+            className={`chat ${message.senderId ? "chat-end" : "chat-start"}`}
+          >
+            <div className="chat-image avatar"></div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ChatContainer;
